10e5f8d27736b05186c41c9f097b3d28e5f77c52
Osman Çelik <osmanceliktr@gmail.com>
2018-03-31 13:05:56 +0300

    Fotoğrafı Metne Çeviren Program (Tesseract Ocr)

---
+++ TesseractOcr/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDosyaSec = new System.Windows.Forms.Button();
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDosyaSec
+            // 
+            this.btnDosyaSec.Location = new System.Drawing.Point(319, 12);
+            this.btnDosyaSec.Name = "btnDosyaSec";
+            this.btnDosyaSec.Size = new System.Drawing.Size(150, 36);
+            this.btnDosyaSec.TabIndex = 0;
+            this.btnDosyaSec.Text = "Dosya Seç";
+            this.btnDosyaSec.UseVisualStyleBackColor = true;
+            this.btnDosyaSec.Click += new System.EventHandler(this.btnDosyaSec_Click);
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.Location = new System.Drawing.Point(12, 54);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(457, 461);
+            this.pbFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbFoto.TabIndex = 1;
+            this.pbFoto.TabStop = false;
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Location = new System.Drawing.Point(491, 10);
+            this.txtMetin.Multiline = true;
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(437, 505);
+            this.txtMetin.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 520);
+            this.Controls.Add(this.txtMetin);
+            this.Controls.Add(this.pbFoto);
+            this.Controls.Add(this.btnDosyaSec);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fotoğrafı Metne Çeviren Program (Tesseract Ocr)";
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDosyaSec;
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.TextBox txtMetin;
+    }
+}
+
---
+++ TesseractOcr/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDosyaSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Filter ="Resim Dosyaları |*.jpeg;*.jpg;*.png";
+            oFile.InitialDirectory = Application.StartupPath; //var sayılan dizin uygulamamın bulunduğu dizin olsun
+            if (DialogResult.OK == oFile.ShowDialog())
+            {
+                pbFoto.ImageLocation = oFile.FileName; //Seçilen dosyayı görmek içi PictureBox nesnesine atıyoruz.
+
+                //tessdata dizininde dil paketleri bulunmaktadır. Türkçe için tur yazıyoruz.
+                using (var dosya = new TesseractEngine(@"./tessdata", "tur", EngineMode.Default)) 
+                {
+                    using (var img = Pix.LoadFromFile(oFile.FileName))
+                    {
+                        using (var txt = dosya.Process(img))
+                        {
+                            txtMetin.Text = txt.GetText(); 
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
